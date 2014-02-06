0fa1b06bd7faee13b2b12904a087a3868e58e635
Johan <tojohansvensson@gmail.com>
2014-02-06 16:30:47 +0100

    Performance-klass skapad

---
+++ src/dbtLab3/Performance.java
@@ -0,0 +1,16 @@
+package dbtLab3;
+
+public class Performance {
+	String date;
+	String movieName;
+	String theaterName;
+	int nbrOfSeats;
+
+	public Performance(String date, String movieName, String theaterName, int nbrOfSeats){
+		this.date = date;
+		this.movieName = movieName;
+		this.theaterName = theaterName;
+		this.nbrOfSeats = nbrOfSeats;
+	}
+	
+}
